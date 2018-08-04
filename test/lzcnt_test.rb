8d99bdf51c908aa0625d1e8ce119b25b3fe19d69
SenriYoshikawa <tisato8899@gmail.com>
2018-08-04 20:37:46 +0900

    change average

--- test/lzcnt_test.rb
+++ test/lzcnt_test.rb
@@ -1,26 +1,37 @@
 require 'benchmark'
 
 data = [0x000f, 0x00ff, 0x0fff, 0xffff, 0xfff0, 0xff00, 0xf000]
+N = 100
 
 for i in 0 ..6
-
-    result = Benchmark.realtime do
-        cnt = 0
-        for j in 63 .. 0
-            if(data[i][j] == 1) then
-                break;
-            else
-                cnt += 1
+    time = 0
+    for j in 0..N-1
+        result = Benchmark.realtime do
+            cnt = 0
+            for j in 63 .. 0
+                if(data[i][j] == 1) then
+                    break;
+                else
+                    cnt += 1
+                end
             end
-        end
-    end 
-
-    puts "loop time =  #{result}s"
-
-    result = Benchmark.realtime do
-        data[i].popcnt_64()
+        end 
+        time += result
     end
 
-    puts "simd time =  #{result}s"
+    #puts "loop time =  #{result}s"
+    print sprintf('    0x%04x & ', data[i])
+    print "#{time  / N}s & "
+
+    time = 0
+    for j in 0..N-1
+        result = Benchmark.realtime do
+            data[i].popcnt_64()
+        end
+        time += result
+    end
+
+    #puts "simd time =  #{result}s"
+    puts "#{time / N}s \\\\ \\hline"
 
 end
--- test/popcnt_test.rb
+++ test/popcnt_test.rb
@@ -1,24 +1,35 @@
 require 'benchmark'
 
 data = [0x000f, 0x00ff, 0x0fff, 0xffff, 0xfff0, 0xff00, 0xf000]
+N = 100
 
 for i in 0 ..6
-
-    result = Benchmark.realtime do
-        sum = 0
-        for j in 0 .. 63
-            if(data[i][j] == 1) then
-                sum += 1
-            end
-        end 
+    time = 0
+    for j in 0..N-1
+        result = Benchmark.realtime do
+            sum = 0
+            for j in 0 .. 63
+                if(data[i][j] == 1) then
+                    sum += 1
+                end
+            end 
+        end
+        time += result
     end
 
-    puts "loop time =  #{result}s"
+    #puts "loop time =  #{time  / N}s"
+    print sprintf('    0x%04x & ', data[i])
+    print "#{time  / N}s & "
 
-    result = Benchmark.realtime do
-        data[i].popcnt_64()
+    time = 0
+    for j in 0..N-1
+        result = Benchmark.realtime do
+            data[i].popcnt_64()
+        end
+        time += result
     end
 
-    puts "simd time =  #{result}s"
+    #puts "simd time =  #{time / N}s"
+    puts "#{time / N}s \\\\ \\hline"
 
 end
